6039121ba5da9bc0c5d770ce2329b5e461a2b1e3
Ignacio Menna <imenna@frsn.utn.edu.ar>
2024-07-16 23:30:56 -0300

    ...

--- main.js
+++ main.js
@@ -1,53 +1,67 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
 const apiKey1 = "AIzaSyCTVnyDpBA9cdgYoKCKpTHiTv_GYDPLvXE";
 const apiKey2 = "AIzaSyD-2x4yTz6M6n3aXsQ9J5vZDzJjF3cR8F0";
 const apiKey3 = "AIzaSyB-8l0kQ4w2a3bZk1z9m4Mf5Jd0ZqK0Z2U";
 
-let nombreBot = document.querySelector('body').getAttribute('data-nombre-bot');
-let prompt = null;
-let imgBot = null;
+let nombreBot, prompt, imgBot;
+let genAI, model, chat;
 
-//Genera el bot y lo configura
-const apis = [apiKey1, apiKey2, apiKey3];
-let indice = 0;
-let genAI = new GoogleGenerativeAI(apis[indice]);
+document.addEventListener("DOMContentLoaded", function() {
+    nombreBot = window.nombreBot;
+    prompt = window.prompt;
+    imgBot = window.imgBot;
 
-const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash",
-    systemInstruction: prompt,
+    console.log("Nombre del Bot:", nombreBot);
+    console.log("Prompt del Bot:", prompt);
+    console.log("Imagen del Bot:", imgBot);
+
+    // Inicializa el bot y configura GoogleGenerativeAI
+    const apis = [apiKey1, apiKey2, apiKey3];
+    let indice = 0;
+    genAI = new GoogleGenerativeAI(apis[indice]);
+
+    model = genAI.getGenerativeModel({ 
+        model: "gemini-1.5-flash",
+        systemInstruction: prompt,
+    });
+    chat = model.startChat({
+        history: [],
+    });
+
+    // Carga los datos del chat desde el localstorage
+    loadDataFromLocalstorage();
 });
-const chat = model.startChat({
-    history: [],
-});
-  
+
 const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
-const API_KEY = genAI.apiKey;
+const API_KEY = genAI ? genAI.apiKey : null;
 
-//Copia el texto de respuesta del chatbot
+// Copia el texto de respuesta del chatbot
 const copyResponse = (copyBtn) => {
     const reponseTextElement = copyBtn.parentElement.querySelector("p");
     navigator.clipboard.writeText(reponseTextElement.textContent);
     copyBtn.textContent = "done";
     setTimeout(() => copyBtn.textContent = "content_copy", 1000);
 }
-//Envia el mensaje del usuario al presionar el boton de enviar
+
+// Envía el mensaje del usuario al presionar el botón de enviar
 sendButton.addEventListener("click", () => {
     handleOutgoingChat();
 });
 
-//Copia el texto de respuesta del chatbot al presionar el boton de copiar
+// Copia el texto de respuesta del chatbot al presionar el botón de copiar
 chatContainer.addEventListener("click", (e) => {
     if (e.target.classList.contains("material-symbols-rounded")) {
         copyResponse(e.target);
     }
 });
 
-//Carga los datos del chat desde el localstorage
+// Carga los datos del chat desde el localstorage
 const loadDataFromLocalstorage = () => {
     const themeColor = localStorage.getItem("themeColor");
     
@@ -57,14 +71,13 @@ const loadDataFromLocalstorage = () => {
     const defaultText = `<div class="default-text">
     <h1>ArgenBot</h1>
     <p>Empieza una conversacion con ${nombreBot}.<br> El historial del chat sera mostrado aqui.</p>
-    </div>`
+    </div>`;
     
     chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
 
-loadDataFromLocalstorage();
-//Crea un elemento de chat
+// Crea un elemento de chat
 const createChatElement = (content, className) => {
     const chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
@@ -73,58 +86,59 @@ const createChatElement = (content, className) => {
 }
 
 let userText = null;
-//Obtiene la respuesta del chatbot
+
+// Obtiene la respuesta del chatbot
 const getChatResponse = async (incomingChatDiv) => {
-    prompt = userText;  // Usamos el texto del usuario como prompt para generar contenido
+    const promptText = userText;  // Usamos el texto del usuario como prompt para generar contenido
 
-  try {
-      // Generamos contenido usando el modelo obtenido previamente
-      const result = await chat.sendMessage(prompt);
-      const generatedText = result.response.text();
+    try {
+        // Generamos contenido usando el modelo obtenido previamente
+        const result = await chat.sendMessage(promptText);
+        const generatedText = result.response.text();
 
-      // Creamos un elemento <p> para mostrar la respuesta generada
-      const pElement = document.createElement("p");
-      pElement.textContent = generatedText;
+        // Creamos un elemento <p> para mostrar la respuesta generada
+        const pElement = document.createElement("p");
+        pElement.textContent = generatedText;
 
-      // Actualizamos la interfaz de usuario con la respuesta generada
-      incomingChatDiv.querySelector(".typing-animation").remove();
-      incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
-      localStorage.setItem("all-chats", chatContainer.innerHTML);
-      chatContainer.scrollTo(0, chatContainer.scrollHeight);
+        // Actualizamos la interfaz de usuario con la respuesta generada
+        incomingChatDiv.querySelector(".typing-animation").remove();
+        incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+        localStorage.setItem("all-chats", chatContainer.innerHTML);
+        chatContainer.scrollTo(0, chatContainer.scrollHeight);
 
-  } catch (error) {
-      if(indice < 2){
-        indice++;
-      }else{
-        indice = 0;
-      }
-      genAI = new GoogleGenerativeAI(apis[indice]);
-      // Manejamos errores en caso de que falle la generación de contenido
-      const pElement = document.createElement("p");
-      pElement.classList.add("error");
-      pElement.textContent = "Algo anduvo mal. Por favor, intenta de nuevo.";
+    } catch (error) {
+        if (indice < 2) {
+            indice++;
+        } else {
+            indice = 0;
+        }
+        genAI = new GoogleGenerativeAI(apis[indice]);
+        // Manejamos errores en caso de que falle la generación de contenido
+        const pElement = document.createElement("p");
+        pElement.classList.add("error");
+        pElement.textContent = "Algo anduvo mal. Por favor, intenta de nuevo.";
 
-      incomingChatDiv.querySelector(".typing-animation").remove();
-      incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
-      localStorage.setItem("all-chats", chatContainer.innerHTML);
-      chatContainer.scrollTo(0, chatContainer.scrollHeight);
-  }
+        incomingChatDiv.querySelector(".typing-animation").remove();
+        incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+        localStorage.setItem("all-chats", chatContainer.innerHTML);
+        chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    }
 }
 
-//Muestra la animacion de escritura
+// Muestra la animación de escritura
 const showTypingAnimation = () => {
-  const html = `<div class="chat-content">
-  <div class="chat-details">
-  <img src="${imgBot}" alt="chatbot-img">
-  <div class="typing-animation">
-  <div class="typing-dot" style="--delay: 0.2s"></div>
-  <div class="typing-dot" style="--delay: 0.3s"></div>
-  <div class="typing-dot" style="--delay: 0.4s"></div>
-  </div>
-  </div>
-  <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
-  </div>`;
-  
+    const html = `<div class="chat-content">
+    <div class="chat-details">
+    <img src="${imgBot}" alt="chatbot-img">
+    <div class="typing-animation">
+    <div class="typing-dot" style="--delay: 0.2s"></div>
+    <div class="typing-dot" style="--delay: 0.3s"></div>
+    <div class="typing-dot" style="--delay: 0.4s"></div>
+    </div>
+    </div>
+    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+    </div>`;
+    
     const incomingChatDiv = createChatElement(html, "incoming");
     chatContainer.appendChild(incomingChatDiv);
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
@@ -133,30 +147,30 @@ const showTypingAnimation = () => {
 
 const initialInputHeight = chatInput.scrollHeight;
 
-//Maneja el chat saliente
+// Maneja el chat saliente
 const handleOutgoingChat = () => {
-  userText = chatInput.value.trim();
-  if(!userText) return;
-  
-  chatInput.value = "";
-  chatInput.style.height = `${initialInputHeight}px`;
-  
-  const html = `<div class="chat-content">
-  <div class="chat-details">
-  <img src="images/user.svg" alt="user-img">
-  <p>${userText}</p>
-  </div>
-  </div>`;
-  
-  const outgoingChatDiv = createChatElement(html, "outgoing");
-  chatContainer.querySelector(".default-text")?.remove();
-  chatContainer.appendChild(outgoingChatDiv);
-  chatContainer.scrollTo(0, chatContainer.scrollHeight);
-  setTimeout(showTypingAnimation, 500);
+    userText = chatInput.value.trim();
+    if (!userText) return;
+    
+    chatInput.value = "";
+    chatInput.style.height = `${initialInputHeight}px`;
+    
+    const html = `<div class="chat-content">
+    <div class="chat-details">
+    <img src="images/user.svg" alt="user-img">
+    <p>${userText}</p>
+    </div>
+    </div>`;
+    
+    const outgoingChatDiv = createChatElement(html, "outgoing");
+    chatContainer.querySelector(".default-text")?.remove();
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
 }
 
 deleteButton.addEventListener("click", () => {
-    if(confirm("Seguro queres eliminar todo el chat?")) {
+    if (confirm("Seguro queres eliminar todo el chat?")) {
         localStorage.removeItem("all-chats");
         loadDataFromLocalstorage();
     }
@@ -168,9 +182,9 @@ themeButton.addEventListener("click", () => {
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 });
 
-//Ajusta la altura del input de chat
+// Ajusta la altura del input de chat
 chatInput.addEventListener("input", () => {
-    chatInput.style.height =  `${initialInputHeight}px`;
+    chatInput.style.height = `${initialInputHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
@@ -180,4 +194,3 @@ chatInput.addEventListener("keydown", (e) => {
         handleOutgoingChat();
     }
 });
-
